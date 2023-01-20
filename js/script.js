bedc8c34a6ec47f1c60c57997b03d8e2f689c3bb
Dyego Anjos <107328787+DyegoAnjos@users.noreply.github.com>
2023-01-20 16:34:19 -0300

    Js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,20 @@
+const boxForm = document.getElementById("form")
+const inputNamePokemon = document.getElementById("namePokemon")
+
 const divAbout = document.getElementById("divAbout")
+const divBase = document.getElementById("divBase")
+const divMoves = document.getElementById("divMoves")
+
+const imgPokemon =document.querySelector("#pokemon")
+
+const divTypes = document.querySelectorAll("#divAbout .types")
 const heightPokemon = document.getElementById("height")
 const weightPokemon = document.getElementById("weight")
 const abilitiesPokemon = document.getElementById("abilities")
 
 
-const divBase = document.getElementById("divBase")
-const divMoves = document.getElementById("divMoves")
-
-const boxForm = document.getElementById("form")
-const inputNamePokemon = document.getElementById("namePokemon")
-
+const attributeNameArray = document.querySelectorAll(".attributeName")
+const levelIcon = document.querySelectorAll(".level")
 
 
 function switchDisplayDiv(id){
@@ -42,10 +47,22 @@ const pickInfsPokemon = async(pokemon) =>{
     return data
 }
 
-const changeInfsPokemon = async(pokemon) =>{
-    const data =await pickInfsPokemon(pokemon)
+const aboutSwitch = (data) =>{
+    for(let i=0;i<=data.types.length-1;i++){
+        const typeCardCreat = document.createElement("div")
+        typeCardCreat.setAttribute("class", "typeCard")
 
-    
+        divTypes[0].appendChild(typeCardCreat)
+
+        const typeCard=document.querySelectorAll("#divAbout .types .typeCard")
+        
+        const typeNameCreat =document.createElement("p")
+        typeNameCreat.setAttribute("class", "typeName")
+
+        typeNameCreat.innerText = data.types[i].type.name
+
+        typeCard[i].appendChild(typeNameCreat)
+    }
 
     heightPokemon.innerText = "Altura: "+data.height/10+"m"
     weightPokemon.innerText = "Peso: "+data.weight/10+".0kg"
@@ -65,8 +82,25 @@ const changeInfsPokemon = async(pokemon) =>{
     
         
     }
-        
+}
 
+const baseSwitch = (data) =>{
+    for(let i=0; i<=data.stats.length-1;i++){
+        attributeNameArray[i].innerText += data.stats[i].base_stat
+
+        for(let j=0;j<Math.round(data.stats[i].base_stat/10) && j<10;j++){
+            levelIcon[j+i*10].classList.add("full")
+        }
+    }
+}
+
+const changeInfsPokemon = async(pokemon) =>{
+    const data =await pickInfsPokemon(pokemon)
+
+    imgPokemon.setAttribute("src", data.sprites.other.home.front_default)
+
+    aboutSwitch(data)
+    baseSwitch(data)
 }
 
 boxForm.addEventListener("submit", (e) => {
@@ -74,6 +108,4 @@ boxForm.addEventListener("submit", (e) => {
     const namePokemon = inputNamePokemon.value.substr(0).toLowerCase()
 
     changeInfsPokemon(namePokemon)
-
-
 })
